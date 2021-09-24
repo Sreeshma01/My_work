89add618287593ee3144f04451a06b73dbc3b481
Sreeshma <sreeshmamsatheesh95@gmail.com>
2021-09-24 05:34:00 -0700

    adding java file

---
+++ ArraySum.java
@@ -0,0 +1,18 @@
+class ArraySum
+{
+public static void main(String args[])
+{
+ int array[];
+array=new int[4];
+array[0]=10;
+array[1]=20;
+array[2]=5;
+array[3]=2;
+int sum=0;
+for(int i=0;i<array.length;i++)
+{
+sum+=array[i];
+}
+System.out.println(sum);
+}
+}
