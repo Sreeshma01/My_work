ae61e79bc5d04920a41ead74fa8d3e3a3ff35fe5
Sreeshma <sreeshmamsatheesh95@gmail.com>
2021-09-24 04:41:44 -0700

    initial commit

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Add{
+public static void main(String args[]){
+ Scanner sb=new Scanner(System.in);
+System.out.println("Enter the first number");
+int a=sb.nextInt();
+System.out.println("Enter the second number");
+int b=sb.nextInt();
+int c=a+b;
+System.out.println("Sum is" +c);
+}
+
+}
+
